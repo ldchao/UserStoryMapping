beff5b8b26b59985994879f10ac9d1126c313a83
SummerZW <814298524@qq.com>
2019-02-02 14:59:27 +0800

    release模块测试完毕

--- src/main/java/cn/edu/nju/user_story_mapping/vo/ReleaseVO.java
+++ src/main/java/cn/edu/nju/user_story_mapping/vo/ReleaseVO.java
@@ -1,6 +1,5 @@
 package cn.edu.nju.user_story_mapping.vo;
 
-import cn.edu.nju.user_story_mapping.entity.ActivityEntity;
 import cn.edu.nju.user_story_mapping.entity.ReleaseEntity;
 import lombok.Data;
 
@@ -9,18 +8,20 @@ import java.sql.Timestamp;
 @Data
 public class ReleaseVO {
 
-    private String rid;
-    private String mid;
+    private int rid;
+    private int mid;
     private Timestamp date;
     private int code;
 
-    public ReleaseVO(){
-
+    public ReleaseVO() {
+        this.code = 0;
     }
 
     public ReleaseVO(ReleaseEntity releaseEntity) {
-        this.mid = releaseEntity.getMid() + "";
+        this.rid = releaseEntity.getId();
+        this.mid = releaseEntity.getMid();
         this.date = releaseEntity.getDate();
+        this.code = 1;
     }
 
 }
